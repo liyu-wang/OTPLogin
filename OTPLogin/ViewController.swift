27728aa4db7bfc8e72d964c837be3a0bdbbbdabb
liyu-wang <liyu.wang@oasis.com>
2019-07-03 16:24:02 +1000

    added the initial project

---
+++ OTPLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTPLogin
+//
+//  Created by Liyu Wang on 3/7/19.
+//  Copyright © 2019 Liyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
